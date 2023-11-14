e17d651467e7a7d012f08ba85d3f9e66c4101dd1
KoviDom <dominik.kovac01@seznam.cz>
2023-11-14 14:11:41 +0100

    Exercises

--- src/DayOfTheWeekChallenge.java
+++ src/DayOfTheWeekChallenge.java
@@ -11,6 +11,17 @@ public class DayOfTheWeekChallenge {
         printDayOfTheWeek(6);
         printDayOfTheWeek(7);
 
+        System.out.println("----------------------------------------------------");
+
+        printWeekDay(0);
+        printWeekDay(1);
+        printWeekDay(2);
+        printWeekDay(3);
+        printWeekDay(4);
+        printWeekDay(5);
+        printWeekDay(6);
+        printWeekDay(7);
+
     }
 
     public static void printDayOfTheWeek(int day) {
@@ -29,6 +40,52 @@ public class DayOfTheWeekChallenge {
         };
 
         System.out.println(day + " stands for " + dayOfWeek);
+
+    }
+
+//    public static void printWeekDay(int day) {
+//
+//        String dayOfWeek = "Invalid Day";
+//
+//        if (day == 0) {
+//            dayOfWeek = "Sunday";
+//        } else if (day == 1) {
+//            dayOfWeek = "Monday";
+//        } else if (day == 2) {
+//            dayOfWeek = "Tuesday";
+//        } else if (day == 3) {
+//            dayOfWeek = "Wednesday";
+//        } else if (day == 4) {
+//            dayOfWeek = "Thursday";
+//        } else if (day == 5) {
+//            dayOfWeek = "Friday";
+//        } else if (day == 6) {
+//            dayOfWeek = "Saturday";
+//        }
+//
+//        System.out.println(day + " stands for " + dayOfWeek);
+//
+//    }
+
+    public static void printWeekDay(int day) {
+
+        if (day == 0) {
+            System.out.println(day + " stands for Sunday");
+        } else if (day == 1) {
+            System.out.println(day + " stands for Monday");
+        } else if (day == 2) {
+            System.out.println(day + " stands for Tuesday");
+        } else if (day == 3) {
+            System.out.println(day + " stands for Wednesday");
+        } else if (day == 4) {
+            System.out.println(day + " stands for Thursday");
+        } else if (day == 5) {
+            System.out.println(day + " stands for Friday");
+        } else if (day == 6) {
+            System.out.println(day + " stands for Saturday");
+        } else {
+            System.out.println(day + " stands for Invalid Day");
+        }
 
     }
 
